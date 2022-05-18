1c1ec389432c0f154a8ae68a0b10b6790cfce414
VijaykumarBhavanur <vijaykumar@gmail.com>
2022-05-18 18:49:20 +0530

    Added: Java Program

---
+++ src/Sample.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Sample {
+
+	// Calculator Program using functions/methods
+	
+	//   add , sub, mul & div(int num1,int num2)
+	
+	public static void main(String[] args) {
+		
+		/* 
+		 *  Get two numbers  
+		 * 
+		 *    Press 1 for add 2 for sub 3 for mul
+		 *    
+		 *    4 for divsion
+		 *    
+		 *    Switch case ()
+		 *    
+		 *    case 1 add(num1,num2)
+		 *             ;
+		 *    case 2
+		 *    case 3
+		 *    case 4
+		 * 
+		 * 
+		 */
+		
+		
+	}
+
+}
